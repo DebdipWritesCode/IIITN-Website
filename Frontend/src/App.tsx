6bc5446ebd318b6729f4cd20df7a1f6857b8ff1a
DebdipWritesCode <debdipmukherjee52@gmail.com>
2025-02-24 23:15:42 +0530

    merged

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -7,6 +7,10 @@ import Admissions from "./pages/Admissions";
 import Students from "./pages/Students";
 import Alumni from "./pages/Alumni";
 import Placements from "./pages/Placements";
+import Clubs from "./pages/StudentsClubs";
+import Fees from "./pages/Fees";
+import Convocation from "./pages/Convocation";
+import Mess from "./pages/mess";
 import NotFound from "./pages/NotFound";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
@@ -18,27 +22,30 @@ interface LayoutProps {
 const Layout: React.FC<LayoutProps> = ({ children }) => (
   <>
     <Navbar />
-    <div className="">{children}</div>
+    <div className="pt-20">{children}</div> {/* Ensure content is below fixed navbar */}
     <Footer />
   </>
 );
 
-
 const App: React.FC = () => {
   return (
-    <Routes>
-      <Route path="*" element={<NotFound />} />
-      <Route path="/" element={<Layout><Home /></Layout>} />
-      <Route path="/academics" element={<Layout><Academics /></Layout>} />
-      <Route path="/governance" element={<Layout><Governance /></Layout>} />
-      <Route path="/admissions" element={<Layout><Admissions /></Layout>} />
-      <Route path="/students" element={<Layout><Students /></Layout>} />
-      <Route path="/alumni" element={<Layout><Alumni /></Layout>} />
-      <Route path="/placements" element={<Layout><Placements /></Layout>} />
-    </Routes>
+    <div className="bg-white min-h-screen">
+      <Routes>
+        <Route path="*" element={<NotFound />} />
+        <Route path="/" element={<Layout><Home /></Layout>} />
+        <Route path="/academics" element={<Layout><Academics /></Layout>} />
+        <Route path="/governance" element={<Layout><Governance /></Layout>} />
+        <Route path="/admissions" element={<Layout><Admissions /></Layout>} />
+        <Route path="/students" element={<Layout><Students /></Layout>} />
+        <Route path="/alumni" element={<Layout><Alumni /></Layout>} />
+        <Route path="/placements" element={<Layout><Placements /></Layout>} />
+        <Route path="/students/clubs" element={<Layout><Clubs /></Layout>} />
+        <Route path="/students/fees" element={<Layout><Fees /></Layout>} />
+        <Route path="/students/convocation-2023" element={<Layout><Convocation /></Layout>} />
+        <Route path="/students/student-mess" element={<Layout><Mess /></Layout>} />
+      </Routes>
+    </div>
   );
 };
-
-export default App;
 
 export default App;
