21fd4fa277d11eed1a9a2ab36eef19d2799c4357
NerdSalad <krishprakash1232@gmail.com>
2025-02-25 00:36:51 +0530

    member login page

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -14,6 +14,7 @@ import Mess from "./pages/mess";
 import NotFound from "./pages/NotFound";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import AlumniLogin from "./pages/AlumniLogin";
 
 interface LayoutProps {
   children: ReactNode;
@@ -43,6 +44,8 @@ const App: React.FC = () => {
         <Route path="/students/fees" element={<Layout><Fees /></Layout>} />
         <Route path="/students/convocation-2023" element={<Layout><Convocation /></Layout>} />
         <Route path="/students/student-mess" element={<Layout><Mess /></Layout>} />
+        <Route path="/alumni/login" element={<Layout><AlumniLogin /></Layout>} />
+
       </Routes>
     </div>
   );
